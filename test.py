42f38b3817cd6ce6026a79f03a8cd5e06dd3a5d1
lemtiel93 <alessandro.argentino93@gmail.com>
2024-02-20 18:25:15 +0100

    test

---
+++ test.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import random
+from selenium.webdriver.common.action_chains import ActionChains
+
+chrome_options = webdriver.ChromeOptions(); 
+chrome_options.add_experimental_option("excludeSwitches", ['enable-logging'])
+chrome_options.add_experimental_option("detach", True)
+chrome_options.add_argument('--start-maximized')
+chrome_options.add_argument("--enable-javascript")
+chrome_options.add_argument("--enable-cookies")
+
+url = "https://www.booking.com/"
+driver = webdriver.Chrome(options=chrome_options)
+try:
+    driver.get(url)
+except:
+    driver.quit()
