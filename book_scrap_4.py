f3a1ca7453467acf172c433e1da3ec24d1e20961
saghia <ing.andreasaggio@gmail.com>
2024-02-22 17:56:57 +0100

    wip mobile analyze

--- book_scrap_4.py
+++ book_scrap_4.py
@@ -51,15 +51,16 @@ chrome_options.add_argument(f'user-agent={fake_user_agent}')
 #per bypassare errore certificato
 chrome_options.add_experimental_option("excludeSwitches", ['enable-logging'])
 chrome_options.add_experimental_option("detach", True)
-chrome_options.add_argument('--start-maximized')
+#chrome_options.add_argument('--start-maximized')
 chrome_options.add_argument("--enable-javascript")
 
 #chiedo in input città e date asdasd
 citta = input("Citta:")
+if len(citta)<1: citta = "Palermo"
 datain= input("Check-in:")
-if len(datain)<1: datain = "2024-02-24"
+if len(datain)<1: datain = "2024-02-28"
 dataout= input("Check-out:")
-if len(dataout)<1: dataout = "2024-02-25"
+if len(dataout)<1: dataout = "2024-02-29"
 
 userlist =["sunnytraveler@libero.it","pantilaura56@gmail.com"]
 username = random.choice(userlist)
@@ -73,130 +74,200 @@ except:
     print("ehi sono qua")
     #windscribe("disconnect")
     driver.quit()
-try:
-    wait = WebDriverWait(driver, 10)
-    cookie_banner = wait.until(EC.visibility_of_element_located((By.ID,'onetrust-reject-all-handler' )))
-    cookie_banner = driver.find_element(By.ID,'onetrust-accept-btn-handler')
-    ActionChains(driver).move_to_element(cookie_banner).perform()
-    cookie_banner.click()
 
-except:
-   pass
-sleep()
+#INIZIARE QUA DISTINZIONE FRA MOBILE E DESKTOP
+#   1 -> desktop . 0 -> mobile
+    
+if choice == 1 :
+    print("desktop!")
+    try:
+        wait = WebDriverWait(driver, 10)
+        cookie_banner = wait.until(EC.visibility_of_element_located((By.ID,'onetrust-reject-all-handler' )))
+        cookie_banner = driver.find_element(By.ID,'onetrust-accept-btn-handler')
+        ActionChains(driver).move_to_element(cookie_banner).perform()
+        cookie_banner.click()
 
-close_genius()
-#blocco che effettua accesso utente 
-'''
-try: 
-    if username is None:
+    except:
         pass
-    else:
-        accedi = driver.find_element(By.CSS_SELECTOR,'a[data-testid="header-sign-in-button"]')
-        ActionChains(driver).move_to_element(accedi).perform()
-        accedi.click()
-        sleep()
-        email = driver.find_element(By.CSS_SELECTOR,'input[type="email"]')
-        ActionChains(driver).move_to_element(email).perform()
-        email.click()
-        for char in username: 
-            email.send_keys(char)
+    sleep()
+
+    close_genius()
+    #blocco che effettua accesso utente 
+    '''
+    try: 
+        if username is None:
+            pass
+        else:
+            accedi = driver.find_element(By.CSS_SELECTOR,'a[data-testid="header-sign-in-button"]')
+            ActionChains(driver).move_to_element(accedi).perform()
+            accedi.click()
+            sleep()
+            email = driver.find_element(By.CSS_SELECTOR,'input[type="email"]')
+            ActionChains(driver).move_to_element(email).perform()
+            email.click()
+            for char in username: 
+                email.send_keys(char)
+                time.sleep(random.uniform(0.1,0.25))
+            sleep()
+            submit = driver.find_element(By.CSS_SELECTOR,'button[type="submit"]')
+            ActionChains(driver).move_to_element(email).move_to_element(submit).click().perform()
+            sleep()
+            passw = driver.find_element(By.CSS_SELECTOR,'input[type="password"]')
+            ActionChains(driver).move_to_element(passw).perform()
+            passw.click()
+            for char in password:
+                passw.send_keys(char)
+                time.sleep(random.uniform(0.1,0.25))
+            sleep()
+            submit = driver.find_element(By.CSS_SELECTOR,'button[type="submit"]')
+            ActionChains(driver).move_to_element(passw).move_to_element(submit).click().perform()
+            input("premi invio per continuare")
+            driver.quit()
+    except:pass 
+    '''
+    try:     #eseguo ricerca
+        search = driver.find_element(By.ID,':re:')
+        for char in citta:
+            search.send_keys(char)
             time.sleep(random.uniform(0.1,0.25))
         sleep()
-        submit = driver.find_element(By.CSS_SELECTOR,'button[type="submit"]')
-        ActionChains(driver).move_to_element(email).move_to_element(submit).click().perform()
+        data = driver.find_element(By.CSS_SELECTOR,'div[data-testid="searchbox-dates-container"]')
+        data.click()
         sleep()
-        passw = driver.find_element(By.CSS_SELECTOR,'input[type="password"]')
-        ActionChains(driver).move_to_element(passw).perform()
-        passw.click()
-        for char in password:
-            passw.send_keys(char)
-            time.sleep(random.uniform(0.1,0.25))
+        date = driver.find_element(By.CSS_SELECTOR,'span[data-date="'+datain+'"]')
+        date.click()
         sleep()
-        submit = driver.find_element(By.CSS_SELECTOR,'button[type="submit"]')
-        ActionChains(driver).move_to_element(passw).move_to_element(submit).click().perform()
-        input("premi invio per continuare")
+        date = driver.find_element(By.CSS_SELECTOR,'span[data-date="'+dataout+'"]')
+        date.click()
+        sleep()
+        button = driver.find_element(By.CSS_SELECTOR,'button[type="submit"]')
+        button.click()
+    except: 
+        print("non trovo i dati barra di ricerca")
+        #windscribe("disconnect")
         driver.quit()
-except:pass 
-'''
-try:     #eseguo ricerca
-    search = driver.find_element(By.ID,':re:')
-    for char in citta:
-        search.send_keys(char)
-        time.sleep(random.uniform(0.1,0.25))
+        print("error")
+
     sleep()
-    data = driver.find_element(By.CSS_SELECTOR,'div[data-testid="searchbox-dates-container"]')
-    data.click()
+
+    close_genius()
+    numero_pagine = trova_num_pagine()
+
+    dati_hotel=[]
+    html_content = ""
+    for pagina in range(numero_pagine):
+        print("scansiono pagina:",pagina+1)
+        sleep()
+        close_genius()
+        html = driver.page_source
+        html_content += html +"/n/n"
+        hotel_per_pagina = driver.find_elements(By.CSS_SELECTOR,'div[data-testid=property-card]')
+        for hotel in hotel_per_pagina:
+            nome= hotel.find_element(By.CSS_SELECTOR,'div[data-testid="title"]').text
+            prezzo = hotel.find_element(By.CSS_SELECTOR,'span[data-testid="price-and-discounted-price"]').text
+            citta = hotel.find_element(By.CSS_SELECTOR, 'span[data-testid="address"]').text
+            #SIA PUNTEGGIO CHE NUM_RECENSIONI POSSONO NON ESSERCI IN CASO DI NUOVO CLIENTE 
+            #PUNTEGGIO TRAMITE IL DIV a3b8729ab1 d86cee9b25
+            try:
+                # Prova a estrarre il punteggio se presente
+                punteggio = hotel.find_element(By.CSS_SELECTOR, 'div[class="a3b8729ab1 d86cee9b25"]').text
+            except NoSuchElementException:
+                print("nessun punteggio")
+                punteggio = None
+                pass
+            #NUMERO RECENSIONI TRAMITE IL DIV  abf093bdfe f45d8e4c32 d935416c47
+            try:
+                # Prova a estrarre il numero di recensioni se presente
+                num_recensioni = hotel.find_element(By.CSS_SELECTOR, 'div[class="abf093bdfe f45d8e4c32 d935416c47"]').text
+            except NoSuchElementException:
+                print("nessun num_recensioni")
+                num_recensioni = None
+                pass
+            #DISTANZA DAL CENTRO
+            distanza_centro = hotel.find_element(By.CSS_SELECTOR, 'span[data-testid="distance"]').text
+            dati_hotel.append((nome,prezzo,citta,datain,punteggio,num_recensioni,distanza_centro))
+            #print di check
+            #print(dati_hotel)
+        #esco dal ciclo dopo che scansiono ultima pagina 
+        if pagina == numero_pagine-1:
+            break
+        try:
+            next_page = driver.find_element(By.CSS_SELECTOR,'button[aria-label="pagina successiva"]')
+        except:
+            driver.quit()
+        #esco dal ciclo dopo che scansiono ultima pagina    
+        
+        next_page.click()
+
+    with open(citta+"-"+datain+".html", "w", encoding="utf-8") as file:
+        file.write(html_content)
+
+    print(dati_hotel)  
+    print("Hotel scansionati",len(dati_hotel)) 
+
     sleep()
-    date = driver.find_element(By.CSS_SELECTOR,'span[data-date="'+datain+'"]')
-    date.click()
-    sleep()
-    date = driver.find_element(By.CSS_SELECTOR,'span[data-date="'+dataout+'"]')
-    date.click()
-    sleep()
-    button = driver.find_element(By.CSS_SELECTOR,'button[type="submit"]')
-    button.click()
-except: 
-    print("non trovo i dati barra di ricerca")
-    #windscribe("disconnect")
     driver.quit()
-    print("error")
 
-sleep()
+elif choice == 0 :
+    print("telefono!")
+    try:
+        wait = WebDriverWait(driver, 10)
+        cookie_banner = wait.until(EC.visibility_of_element_located((By.ID,'onetrust-reject-all-handler' )))
+        cookie_banner = driver.find_element(By.ID,'onetrust-accept-btn-handler')
+        ActionChains(driver).move_to_element(cookie_banner).perform()
+        cookie_banner.click()
+    except:
+        pass
 
-close_genius()
-numero_pagine = trova_num_pagine()
-
-dati_hotel=[]
-html_content = ""
-for pagina in range(numero_pagine):
-    print("scansiono pagina:",pagina+1)
     sleep()
     close_genius()
-    html = driver.page_source
-    html_content += html +"/n/n"
-    hotel_per_pagina = driver.find_elements(By.CSS_SELECTOR,'div[data-testid=property-card]')
-    for hotel in hotel_per_pagina:
-        nome= hotel.find_element(By.CSS_SELECTOR,'div[data-testid="title"]').text
-        prezzo = hotel.find_element(By.CSS_SELECTOR,'span[data-testid="price-and-discounted-price"]').text
-        citta = hotel.find_element(By.CSS_SELECTOR, 'span[data-testid="address"]').text
-        #SIA PUNTEGGIO CHE NUM_RECENSIONI POSSONO NON ESSERCI IN CASO DI NUOVO CLIENTE 
-        #PUNTEGGIO TRAMITE IL DIV a3b8729ab1 d86cee9b25
-        try:
-            # Prova a estrarre il punteggio se presente
-            punteggio = hotel.find_element(By.CSS_SELECTOR, 'div[class="a3b8729ab1 d86cee9b25"]').text
-        except NoSuchElementException:
-            print("nessun punteggio")
-            punteggio = None
-            pass
-        #NUMERO RECENSIONI TRAMITE IL DIV  abf093bdfe f45d8e4c32 d935416c47
-        try:
-            # Prova a estrarre il numero di recensioni se presente
-            num_recensioni = hotel.find_element(By.CSS_SELECTOR, 'div[class="abf093bdfe f45d8e4c32 d935416c47"]').text
-        except NoSuchElementException:
-            print("nessun num_recensioni")
-            num_recensioni = None
-            pass
-        #DISTANZA DAL CENTRO
-        distanza_centro = hotel.find_element(By.CSS_SELECTOR, 'span[data-testid="distance"]').text
-        dati_hotel.append((nome,prezzo,citta,datain,punteggio,num_recensioni,distanza_centro))
-        #print di check
-        #print(dati_hotel)
-    #esco dal ciclo dopo che scansiono ultima pagina 
-    if pagina == numero_pagine-1:
-        break
-    try:
-        next_page = driver.find_element(By.CSS_SELECTOR,'button[aria-label="pagina successiva"]')
-    except:
+
+    try:     #eseguo ricerca mobile
+        search = driver.find_element(By.ID,':r8:')
+        search.click()
+        for char in citta:
+            search.send_keys(char)
+            time.sleep(random.uniform(0.1,0.25))
+        close_city = driver.find_element(By.CSS_SELECTOR,'button[class="a83ed08757 c21c56c305 f38b6daa18 d691166b09 ab98298258 deab83296e f4552b6561"]')
+        close_city.click()
+        sleep()
+        data = driver.find_element(By.CSS_SELECTOR,'button[class="b8118a93a7"]')
+        data.click()
+        sleep()
+        sleep()
+        sleep()
+        date = driver.find_element(By.CSS_SELECTOR,'span[data-date="'+datain+'"]')
+        date.click()
+        sleep()
+        date = driver.find_element(By.CSS_SELECTOR,'span[data-date="'+dataout+'"]')
+        date.click()
+        sleep()
+        # click fatto dopo scelta data -- init
+        btn_fatto = driver.find_element(By.CSS_SELECTOR,'button[class="a83ed08757 c21c56c305 a4c1805887 f671049264 d2529514af c082d89982"]')
+        btn_fatto.click()
+        # click fatto -- end
+        sleep()
+        button = driver.find_element(By.CSS_SELECTOR,'button[type="submit"]')
+        button.click()
+    except: 
+        print("non trovo i dati barra di ricerca")
+        #windscribe("disconnect")
         driver.quit()
-    #esco dal ciclo dopo che scansiono ultima pagina    
-    
-    next_page.click()
+        print("error")
 
-with open(citta+"-"+datain+".html", "w", encoding="utf-8") as file:
-    file.write(html_content)
+    sleep()
+    sleep()
 
-print(dati_hotel)  
-print("Hotel scansionati",len(dati_hotel)) 
+    close_genius()
+    numero_pagine = trova_num_pagine()
 
-sleep()
-driver.quit()
+    hotel_per_pagina = driver.find_elements(By.CSS_SELECTOR,'div[data-testid=property-card]')
+    print(len(hotel_per_pagina))  # Utilizzo len() per ottenere la lunghezza della lista
+    for hotel in hotel_per_pagina:
+        print(hotel)
+
+
+
+
+else :
+    print("error")
